9e220e5c4ae8ca0c92ba65d8006fd99cb6218206
Gabriel Barros <gabriellourencobarros02@gmail.com>
2022-05-24 18:23:20 -0300

    Corrigindo erros

--- FlaskCrudApplication/main.py
+++ FlaskCrudApplication/main.py
@@ -193,6 +193,6 @@ def deslogar():
     return redirect(url_for('login'))
 
 
-# if __name__ == "__main__":
-#     serve(application, host='0.0.0.0', port=80)
-application.run(debug=True)
+if __name__ == "__main__":
+    serve(application, host='0.0.0.0', port=80)
+# application.run(debug=True)
